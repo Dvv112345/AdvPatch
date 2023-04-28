5dcdf12e24b145e6948724b412eaf9bea4e2db35
Victor <dvv112345@gmail.com>
2023-04-28 11:48:51 +0400

    YOLOv4

--- pytorchYOLOv4/weight/download_weights.sh
+++ pytorchYOLOv4/weight/download_weights.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Download weights for vanilla YOLOv4
-wget -c https://pjreddie.com/media/files/yolov4.weights -O ./pyTorchYOLOv4/weight/yolov4.weights
+wget -c https://pjreddie.com/media/files/yolov4.weights -O ./yolov4.weights
 # wget -c https://pjreddie.com/media/files/yolov4.weights
 # # Download weights for tiny YOLOv4
-wget -c https://pjreddie.com/media/files/yolov4-tiny.weights -O ./pyTorchYOLOv4/weight/yolov4-tiny.weights
+wget -c https://pjreddie.com/media/files/yolov4-tiny.weights -O ./yolov4-tiny.weights
 # Download weights for backbone network
 # wget -c https://pjreddie.com/media/files/darknet53.conv.74
